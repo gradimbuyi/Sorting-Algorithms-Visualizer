959876b9ccb19c4212bc056268fe6a01fc31696f
Gradi Mbuyi <gradimbuyi@outlook.com>
2023-03-08 22:12:37 -0500

    Add Selection Sort object

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        Algorithms bubbleSort = new BubbleSort(10);
+        Algorithms bubbleSort = new BubbleSort(50);
+        Algorithms selectionSort = new SelectionSort(50);
+
+        System.out.println("Bubble Sort");
         bubbleSort.printList(); bubbleSort.sortList();
+
+        System.out.println("Seleciton Sort");
+        selectionSort.printList(); selectionSort.sortList();
     }
+
+
 }
