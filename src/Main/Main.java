5fc338cdbb185e4b246bb7ec45157d2fa39a2d67
Gradi Mbuyi <gradimbuyi@outlook.com>
2023-09-14 22:13:03 -0400

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,13 +5,13 @@ import Graphics.VisualizerFrame;
 /**
  * This is a personal project implementing 5 of the sorting algorithms covered in UCF's Computer Science 1
  * course. I built this program to get a better understanding of object-oriented programming, as well as GUI
- * programing. The Graphic package contain codes written to animate and visualize the algorithm in a graphical
- * matter, and the Logic package contain codes written for each algorithm.
+ * programming. The Graphic package contains codes written to animate and visualize the algorithm in a graphical
+ * matter, and the Logic package contains codes written for each algorithm.
  *
  * <p><p><p>
  *
- * The first implementation of this visualizer features: bubble sort, insertion sort, merge sort, quick sort,
- * and selection sort. I attend to add merge insertion sort in the near future, as well as more coloring to visualize
+ * The first implementation of this visualizer features bubble sort, insertion sort, merge sort, quick sort,
+ * and selection sort. I intend to add merge insertion sort in the near future, as well as more coloring to visualize
  * operations such as comparison during the sorting process.
  *
  * @version 1.0
